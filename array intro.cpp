883a8d9e43d176613b84793a4e429aebda997657
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-04-23 14:44:53 +0530

    Create array intro.cpp

---
+++ array intro.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+// void printArray(){
+
+// }
+
+int main(){
+    int number[15];
+    
+    cout<<"Value at Zero index "<<number[1]<<endl;
+    return 0;
+}
