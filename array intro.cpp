b40042dad0afbe807abee2fc442b6e9c617fe4d6
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-04-24 12:55:29 +0530

    ..

--- array intro.cpp
+++ array intro.cpp
@@ -7,7 +7,23 @@ using namespace std;
 
 int main(){
     int number[15];
-    
     cout<<"Value at Zero index "<<number[1]<<endl;
+
+    int second[3]={5,7,11};
+    cout<<"Value at Zero index "<<second[0]<<endl;
+
+    int third[15]={2,7};
+    int n=15;
+    cout<<"prinithing the array"<<endl;
+    for(int i=0;i<=n;i++){
+        cout<<third[i]<<" ";
+    }
+
+    int fourth[10]={0};
+    int m=10;
+    cout<<"printing the array"<<endl;
+    for(int i=0;i<=m;i++){
+        cout<<fourth[i]<<" ";
+    }
     return 0;
 }
